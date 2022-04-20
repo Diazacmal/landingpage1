2356dc10e8143c2348cb8815a61dd7b1cd4ebf99
diazakmal <fallusony@gmail.com>
2022-04-20 16:02:32 +0700

    menambah responsif mobile

---
+++ script.js
@@ -0,0 +1,5 @@
+const kontra = document.querySelector(".panahcinta");
+window.addEventListener("scroll", function() {
+    windowPosition = window.scrollY > 200;
+    kotra.classList.toggle("panahcinta", windowPosition);
+});
